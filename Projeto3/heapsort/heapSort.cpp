911aaa447139ad5d55c9bc13f83d3b3f48ed17ef
hananiasdaniel <hananiasdaniel@hotmail.com>
2018-08-29 22:33:07 -0300

    heapsort e counting sort finalizado + makefile

--- Projeto3/heapsort/heapSort.cpp
+++ Projeto3/heapsort/heapSort.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-void maxHeapify(int array[], int i, int comprimento){
+void maxHeapify(int array[], int comprimento, int i){
+	int maior = i; //o maior valor eh a raiz
 	int sonLeft = 2*i + 1; //filho da esquerda
 	int sonRight = 2*i + 2; //filho da direita
-	int maior = i; //o maior valor eh a raiz
+	int aux = 0;
+
 
 	if(sonLeft < comprimento && array[sonLeft] > array[maior]){
 		maior = sonLeft;
@@ -17,35 +19,42 @@ void maxHeapify(int array[], int i, int comprimento){
 
 	if(maior != i){
 		swap(array[i], array[maior]);
-		maxHeapify(array, maior, comprimento);
+		maxHeapify(array, i, maior);
 	}
 
 }
 
 void buildMaxHeap(int array[], int comprimento){
-	for(int i = ((comprimento/2)-1); i >= 0; i--){
-		maxHeapify(array, i, comprimento);
+	for(int i = (comprimento/2) - 1; i >= 0; i--){
+		maxHeapify(array, comprimento, i);
 	}
 }
 
 void heapSort(int array[], int comprimento){
+	int aux = 0;
 	buildMaxHeap(array, comprimento);
 	for(int i = comprimento - 1; i >= 0; i--){
 		swap(array[0], array[i]);
-		maxHeapify(array, 0, comprimento);
+		maxHeapify(array, i, 0);
 	}
 }
 
-int main(){
-	int array[] = {2, 4, 15, 7, 1, 13, 0, 10, 5};
-	int comprimento = sizeof(array)/sizeof(int); //numero total de elementos do array
+void Print(int array[], int n){
+	for(int i = 0; i < n; i++)
+		cout << array[i] << " ";
+		cout << endl;
+}
 
-	cout << comprimento << endl;
+int main(){
+	int array[] = {2, 4, 15, 7, 1, 13};
+	int comprimento = sizeof(array)/sizeof(array[0]); //numero total de elementos do array
+	//comprimento = (comprimento/2) - 1;
+	//cout << comprimento << endl;
 	heapSort(array, comprimento);
 	cout << "Array ordenado" << endl;
-	for(int i=comprimento-1; i >= 0; i--){
-		cout << array[i] << endl;
-	}
+	
 
-	return 0;
+	Print(array, comprimento);
+
+	//return 0;
 }
