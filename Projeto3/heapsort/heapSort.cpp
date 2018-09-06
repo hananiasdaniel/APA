f0fabc3707147582856b0310629696533d8f7a62
hananiasdaniel <hananiasdaniel@hotmail.com>
2018-09-06 12:17:42 -0300

    heapsort alterado e funcionando

--- Projeto3/heapsort/heapSort.cpp
+++ Projeto3/heapsort/heapSort.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
+void Print(int array[], int n){
+	for(int i = 0; i < n; i++)
+		cout << array[i] << " ";
+		cout << endl;
+}
+
 void maxHeapify(int array[], int comprimento, int i){
+	//cout << comprimento << endl;
+	//cout << i << endl;
 	int maior = i; //o maior valor eh a raiz
 	int sonLeft = 2*i + 1; //filho da esquerda
 	int sonRight = 2*i + 2; //filho da direita
-	int aux = 0;
+
 
 
 	if(sonLeft < comprimento && array[sonLeft] > array[maior]){
@@ -19,7 +27,8 @@ void maxHeapify(int array[], int comprimento, int i){
 
 	if(maior != i){
 		swap(array[i], array[maior]);
-		maxHeapify(array, i, maior);
+		maxHeapify(array, comprimento, maior);
+
 	}
 
 }
@@ -28,10 +37,10 @@ void buildMaxHeap(int array[], int comprimento){
 	for(int i = (comprimento/2) - 1; i >= 0; i--){
 		maxHeapify(array, comprimento, i);
 	}
+	//Print(array, comprimento);
 }
 
 void heapSort(int array[], int comprimento){
-	int aux = 0;
 	buildMaxHeap(array, comprimento);
 	for(int i = comprimento - 1; i >= 0; i--){
 		swap(array[0], array[i]);
@@ -39,20 +48,14 @@ void heapSort(int array[], int comprimento){
 	}
 }
 
-void Print(int array[], int n){
-	for(int i = 0; i < n; i++)
-		cout << array[i] << " ";
-		cout << endl;
-}
-
 int main(){
-	int array[] = {2, 4, 15, 7, 1, 13};
+	int array[] = {3, 7, 1, 12, 5, 9, 10, 2, 0, 3, 4, 11, 6};
 	int comprimento = sizeof(array)/sizeof(array[0]); //numero total de elementos do array
 	//comprimento = (comprimento/2) - 1;
 	//cout << comprimento << endl;
 	heapSort(array, comprimento);
 	cout << "Array ordenado" << endl;
-	
+
 
 	Print(array, comprimento);
 
